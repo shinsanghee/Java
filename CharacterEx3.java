b7c39412b06ad3784dcf3fc16cbf60b1a4e1169f
sanghee Shin <113161106+shinsanghee@users.noreply.github.com>
2022-10-06 04:29:26 +0900

    Create CharacterEx3.java

---
+++ CharacterEx3.java
@@ -0,0 +1,13 @@
+public class CharacterEx3 {
+    public static void main(String[ ] args) {
+        int a = 65;
+        int b = -66;
+
+        char a2 = 65;
+        //char b2 = -66;
+
+        System.out.println((char)a);
+        System.out.println((char)b);
+        System.out.prinln(a2);
+    }
+}
